efdbb54c0a187a1059e0ebadd2859434c3d519eb
danielg2017 <danieledgardo826@gmail.com>
2021-12-16 14:51:48 -0300

    reparacion de estilos y arreglo js

--- js.js
+++ js.js
@@ -1,38 +1,42 @@
-$(document).ready(function () {
-    $("#wiki-search").keyup(function (event) {
-        if (event.keyCode == 13) {
-            $("#wiki").click();
-        }
-    });
-    $(function () {
-        $('[data-toggle="tooltip"]').tooltip();
-    });
-    $("#wiki").on("click", function () {
-        var wikiSearch = $("#wiki-search").val();
-        var url =
-            "https://es.wikipedia.org/w/api.php?action=opensearch&format=json&search=" + wikiSearch + "&callback=?";
-        $.ajax({
-            url: url,
-            type: "GET",
-            async: false,
-            dataType: "json",
-            success: function (data) {
-                $("#output").html("");
-                for (var i = 0; i < data[1].length; i++) {
-                    $("#output").prepend(
-                        "<li><a href=" +
-                            data[3][i] +
-                            ' target="_blank">' +
-                            data[1][i] +
-                            "</a><p>" +
-                            data[2][i] +
-                            "</p></li>"
-                    );
-                }
-            },
-            error: function (errorMessage) {
-                alert("Error");
-            },
-        });
-    });
+var searchText = document.getElementById("text");
+var searchBtn = document.getElementById("searchbutton");
+var result = document.getElementById("result");
+var wikitext = "";
+
+searchText.addEventListener("keypress", function(event){
+        if (event.keyCode === 13) {
+          event.preventDefault();
+          var txt = searchText.value;
+          executeSearch(txt);
+         }
+      });
+ 
+searchBtn.addEventListener("click", function(){
+  var txt = searchText.value;
+  executeSearch(txt);
 });
+
+function executeSearch(txt){
+  var link = "https://es.wikipedia.org/w/api.php?action=opensearch&format=json&origin=*&search=" + txt + "&limit=10";
+  var xhr = new XMLHttpRequest(); 
+  xhr.open('GET', link);
+  xhr.onload = function() {
+    if (xhr.status === 200) {
+      wikitext = JSON.parse(xhr.responseText);
+      showScreen(wikitext);  
+    }
+    else {
+      console.log('Request failed.  Returned status of ' + xhr.status);
+    }
+  };
+  xhr.send();  
+}
+
+function showScreen(wikitext){
+  //console.log(wikitext);
+  var searchOutput = "";
+	for (var i=0; i<wikitext[1].length; i++) {
+	  searchOutput = searchOutput + "<a href='"+ wikitext[3][i] +"' target='_blank'><div class='entry'><span class='title'>" + wikitext[1][i] + "</span><br/><span class='snippet'>" + wikitext[2][i] + "</span></div></a>";
+  };
+  result.innerHTML = searchOutput;
+};
